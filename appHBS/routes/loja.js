59f63f71ee01a06043d335f0247246d2a4b12f74
Vinicius Lessa Magagnin <viniciuslessamagagnin123@gmail.com>
2026-03-19 11:51:45 -0300

    Versão com o uso de vetores, loja/bermudas modificada usando vetor

--- appHBS/routes/loja.js
+++ appHBS/routes/loja.js
@@ -1,21 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
+//simulando uma loja, onde cada rota é um produto diferente
+ const produtos = [
+  { id: 1, nome: 'Camiseta', preco: 29.99 },
+  { id: 2, nome: 'Bermuda', preco: 49.99 },
+  { id: 3, nome: 'Tênis', preco: 89.99 }
+];
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('loja', { title: 'Loja' });
 });
 
 router.get('/camisetas', function(req, res, next) {
-  res.render('camisetas', { title: 'Camsisetas' });
+  res.render('camisetas', { produtos });
 });
 
-router.get('/camisetas/mangalonga', function(req, res, next) {
-  res.render('mangalonga', { title: 'Camiseta manga longa' });
-});
+/*router.get('/camisetas/mangalonga', function(req, res, next) {
+  res.render('mangalonga', { produto: produtos[1] });
+});*/
 
 router.get('/bermudas', function(req, res, next) {
-  res.render('bermudas', { title: 'Bermudas' });
+  res.render('bermudas', { produtos });
 });
 
 module.exports = router;
